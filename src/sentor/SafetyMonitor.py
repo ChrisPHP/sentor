49932c8fd5feb3d4c35920bff09fc61e2c41b86e
Adam Binch <abinch@sagarobotics.com>
2022-05-18 10:55:12 +0100

    Sentor reports the tag of the condition that takes the robot out of auto. This tag is published on the topic `/sentor/auto_takeover_reason` with msg type sentor.msg.TagArray. As more than one condition can take the robot out of auto at a given time, this topic in fact reports a list of tags.

--- src/sentor/SafetyMonitor.py
+++ src/sentor/SafetyMonitor.py
@@ -8,6 +8,7 @@ Created on Fri Dec  6 08:51:15 2019
 from __future__ import division
 import rospy
 from std_msgs.msg import Bool
+from sentor.msg import Tag, TagArray
 from std_srvs.srv import SetBool, SetBoolResponse
 from threading import Event
 
@@ -15,7 +16,7 @@ from threading import Event
 class SafetyMonitor(object):
     
     
-    def __init__(self, topic, event_msg, attr, srv, event_cb, invert=False):
+    def __init__(self, topic, event_msg, attr, crit_key, srv, event_cb, invert=False):
         
         timeout = rospy.get_param("~safe_operation_timeout", 10.0)
         rate = rospy.get_param("~safety_pub_rate", 10.0)
@@ -27,21 +28,29 @@ class SafetyMonitor(object):
             self.timeout = 0.1
         
         self.attr = attr
+        self.crit_key = crit_key
         self.event_cb = event_cb
         self.invert = invert
+        
         self.topic_monitors = []
+        self.bad_monitors = []
         
         self.timer = None
         self.safe_operation = False        
         self.safe_msg_sent = False
         self.unsafe_msg_sent = False
+        self.auto = False
         
         self._stop_event = Event()
 
         self.event_msg = event_msg + ": "
+        
+        rospy.Subscriber('auto_mode', Bool, self.auto_mode_cb)
 
         self.safety_pub = rospy.Publisher(topic, Bool, queue_size=10)
         rospy.Timer(rospy.Duration(1.0/rate), self.safety_pub_cb)
+        
+        self.auto_takeover_pub = rospy.Publisher("/sentor/auto_takeover_reason", TagArray, queue_size=5)
         
         rospy.Service("/sentor/" + srv, SetBool, self.srv_cb)
 
@@ -50,12 +59,17 @@ class SafetyMonitor(object):
         self.topic_monitors.append(topic_monitor)
         
         
+    def auto_mode_cb(self, msg):
+        self.auto = msg.data
+        
+        
     def safety_pub_cb(self, event=None):
         
         if not self._stop_event.isSet():
 
             if self.topic_monitors:
                 threads_are_safe = [getattr(monitor, self.attr) for monitor in self.topic_monitors]
+                self.bad_monitors = [i for i, item in enumerate(threads_are_safe) if not item]
                 
                 if self.auto_tagging and all(threads_are_safe) and self.timer is None:
                     self.timer = rospy.Timer(rospy.Duration.from_sec(self.timeout), self.timer_cb, oneshot=True)
@@ -68,6 +82,7 @@ class SafetyMonitor(object):
                     self.safe_operation = False                        
                     if not self.unsafe_msg_sent:
                         self.event_cb(self.event_msg + "FALSE", "error")
+                        self.auto_takeover_cb()
                         self.safe_msg_sent = False
                         self.unsafe_msg_sent = True
 
@@ -84,6 +99,23 @@ class SafetyMonitor(object):
             self.event_cb(self.event_msg + "TRUE", "info")
             self.safe_msg_sent = True
             self.unsafe_msg_sent = False
+            
+            
+    def auto_takeover_cb(self):
+        
+        if self.auto:
+            tags = TagArray()
+            for i in self.bad_monitors:
+                monitor = self.topic_monitors[i]
+                
+                for expr in monitor.conditions:
+                    if monitor.conditions[expr][self.crit_key]:
+                        tag = Tag()
+                        tag.tag = monitor.conditions[expr]["tags"]
+                        tags.tags.append(tag)
+            
+            if tags.tags:             
+                self.auto_takeover_pub.publish(tags)
                                        
         
     def srv_cb(self, req):
