10c268d82884aadba27fb524fb802d29cc02be25
Adam Binch <abinch@sagarobotics.com>
2022-07-04 15:14:40 +0100

    example config updated

--- src/sentor/CustomLambdaExample.py
+++ src/sentor/CustomLambdaExample.py
@@ -13,5 +13,5 @@ class CustomLambda(object):
         pass
         
     def run(self, msg):
-        return msg.data == False
+        return msg.data == "r1-cz"
 #########################################################################################################
